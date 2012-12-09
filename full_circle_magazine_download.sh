f0bf40f70874ee919bc69e13a0aa1e899c53ebf3
cormite <cfernand1977@gmail.com>
2012-12-09 15:16:58 -0800

    Create full_circle_magazine_download.sh
    First commit.
    Feel free to make any modifications.

---
+++ full_circle_magazine_download.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#==========================================================
+# Author: Carlos Fernandez San Millan
+#
+# https://github.com/cormite/full_circle_magazine_download
+#==========================================================
+
+special_files="issuePY01_en.pdf  issuePY02_en.pdf  issuePY03_en.pdf  issuePY04_en.pdf  issueSC01_en.pdf  issueSE01_en.pdf  issueUD01_en.pdf  issueUU01_en.pdf  issueVM01_en.pdf"
+first=1
+last=67
+ROOT_DIR=`pwd`
+
+download_issues () {
+        for i in $(eval echo "{$first..$last}")
+        do
+                wget -c 'http://dl.fullcirclemagazine.org/issue'$i'_en.pdf'
+        done
+}
+
+download_special_editions () {
+        for j in $special_files
+        do
+                wget -c 'http://dl.fullcirclemagazine.org/'$j
+        done
+}
+
+
+if [ -d $ROOT_DIR/Issues ]
+then
+        cd $ROOT_DIR/Issues
+        download_issues
+else
+        mkdir $ROOT_DIR/Issues
+        cd $ROOT_DIR/Issues
+        download_issues
+fi
+
+if [ -d $ROOT_DIR/Special_Editions ]
+then
+        cd $ROOT_DIR/Special_Editions
+        download_special_editions
+else
+        mkdir $ROOT_DIR/Special_Editions
+        cd $ROOT_DIR/Special_Editions
+        download_special_editions
+fi
+
+# LAST LINE -- DO NOT REMOVE
